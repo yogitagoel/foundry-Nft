5a72f8d3ef1bc8800454a7a7760ab1aa42f44498
Yogita <yogitagoel2006@gmail.com>
2025-03-16 08:34:25 +0000

    deploy

---
+++ src/basicNFT.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+pragma solidity ^0.8.18;
+
+contract BasicNFT is ERC721 {
+    uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
+
+    constructor() ERC721("BasicNFT", "BFT") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNFT(string memory tokenUri) public returns (uint256) {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        return s_tokenIdToUri[tokenId];
+    }
+}
