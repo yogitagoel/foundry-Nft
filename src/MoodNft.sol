7c244fb857b648e213fe84a7fb87948a5f84c469
Yogita Goyal <yogitagoel2006@gmail.com>
2025-03-21 15:06:08 +0530

    Update MoodNft.sol

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -1,92 +1,94 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
-pragma solidity ^0.8.18;
+import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {Base64} from "../lib/openzeppelin-contracts/contracts/utils/Base64.sol";
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
-
-contract MoodNft is ERC721 {
-    uint256 private s_tokenCounter;
-    string private s_sadSvgImageUri;
-    string private s_happySvgImageUri;
+contract MoodNft is ERC721, Ownable {
+    error ERC721Metadata__URI_QueryFor_NonExistentToken();
+    error MoodNft__CantFlipMoodIfNotOwner();
 
     enum Mood {
-        HAPPY,
-        SAD
+        SAD,
+        HAPPY
     }
 
-    error MoodNFT__CantFlipMoodIfNotOwner();
+    uint256 private s_tokenCounter;
+    string private s_sadSvgUri;
+    string private s_happySvgUri;
 
-    constructor(
-        string memory sadSvgImageUri,
-        string memory happySvgImageUri
-    ) ERC721("Mood NFT", "MN") {
-        s_sadSvgImageUri = sadSvgImageUri;
-        s_happySvgImageUri = happySvgImageUri;
+
+    mapping(uint256 => Mood) private s_tokenIdToState;
+
+    event CreatedNFT(uint256 indexed tokenId);
+
+    constructor(string memory sadSvgUri, string memory happySvgUri) ERC721("Mood NFT", "MN") Ownable(msg.sender) {
+        s_tokenCounter = 0;
+        s_sadSvgUri = sadSvgUri;
+        s_happySvgUri = happySvgUri;
+        
     }
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
-        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter++;
+        emit CreatedNFT(s_tokenCounter);
     }
 
-    function tokenURI(
-        uint256 tokenId
-    ) public view override returns (string memory) {
-        string memory imageURI;
-        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
-            imageURI = s_happySvgImageUri;
-        } else {
-            imageURI = s_sadSvgImageUri;
+    function flipMood(uint256 tokenId) public {
+        if (getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender) {
+            revert MoodNft__CantFlipMoodIfNotOwner();
         }
 
-        string memory tokenMetadata = string.concat(
-            '{"name: "',
-            name(),
-            '", description: "An NFT that reflects your mood!", "attributes": [{"trait_type": "Mood", "value": 100}], "image": ',
-            imageURI,
-            '"}'
-        );
+        if (s_tokenIdToState[tokenId] == Mood.HAPPY) {
+            s_tokenIdToState[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToState[tokenId] = Mood.HAPPY;
+        }
     }
 
     function _baseURI() internal pure override returns (string memory) {
-        return
-            string(
-                abi.encodePacked(
-                    _baseURI(),
-                    Base64.encode(
-                        bytes(
-                            abi.encodePacked(
-                                '{"name: "',
-                                name(),
-                                '", description: "An NFT that reflects your mood!", "attributes": [{"trait_type": "Mood", "value": 100}], "image": ',
-                                imageURI,
-                                '"}'
-                            )
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        if (ownerOf(tokenId) == address(0)) {
+            revert ERC721Metadata__URI_QueryFor_NonExistentToken();
+        }
+        string memory imageURI = s_happySvgUri;
+
+        if (s_tokenIdToState[tokenId] == Mood.SAD) {
+            imageURI = s_sadSvgUri;
+        }
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '{"name":"',
+                            name(),
+                            '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                            '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                            imageURI,
+                            '"}'
                         )
                     )
                 )
-            );
+            )
+        );
     }
 
-    function flipMood(uint256 tokenId) public view {
-        if (
-            getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender
-        ) {
-            revert MoodNFT__CantFlipMoodIfNotOwner();
-        }
-
-        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
-            s_tokenIdToMood[tokenId] == Mood.SAD;
-        } else {
-            s_tokenIdToMood[tokenId] == Mood.HAPPY;
-        }
+    function getHappySVG() public view returns (string memory) {
+        return s_happySvgUri;
     }
 
-    function svgToImageURI(
-        string memory svg
-    ) public pure returns (string memory) {
-        string memory baseURL = "data:image/svg+xml;base64,";
+    function getSadSVG() public view returns (string memory) {
+        return s_sadSvgUri;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
     }
 }
